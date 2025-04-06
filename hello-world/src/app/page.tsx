6c131703b343c15e911feebc25b8891c0c6fb2d5
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-04-06 12:33:03 +0700

    19-Link Component

--- hello-world/src/app/page.tsx
+++ hello-world/src/app/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Link from "next/link"
 
 // type Props = {}
 
 export default function Home() {
   return (
+    <>
     <h1>Home</h1>
+    <Link href="/blog">Blog</Link>
+    <Link href="/products"> Products</Link>
+    </>
   )
 }
--- hello-world/src/app/products/page.tsx
+++ hello-world/src/app/products/page.tsx
@@ -1,9 +1,15 @@
-// import React from 'react'
+import Link from "next/link"
 
 // type Props = {}
 
 export default function Products() {
   return (
+    <>
+    <Link href="/">Home</Link>
     <h1>Products</h1>
+    <Link href="/products/1">Product 1</Link>
+    <Link href="/products/2">Product 2</Link>
+    <Link href="/products/3" replace>Product 3</Link>
+    </>
   )
 }
